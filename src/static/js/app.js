5509772035bff5276384a17038307b2cfa9aa7cc
masaki12-s <70832772+masaki12-s@users.noreply.github.com>
2023-01-11 12:24:02 +0900

    add header

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,7 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (     
         <Container>
-            <h1>ToDo App by Masaki Sanada</h1>
+            ToDo App by Masaki Sanada
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
